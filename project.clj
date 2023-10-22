29f53c8a7f833f54928bf3874b1a586b1537bd92
Stephen Hopper <stephenmhopper@gmail.com>
2023-10-22 15:04:25 -0500

    Add actual implementation with some tests

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject clj-pool-party "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Minimalistic Clojure object pooling library"
+  :url "https://github.com/enragedginger/clj-pool-party"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/test.check "1.1.1"]]
   :main ^:skip-aot clj-pool-party.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]]}})
--- test/clj_pool_party/core_test.clj
+++ test/clj_pool_party/core_test.clj
@@ -1,7 +1,66 @@
 (ns clj-pool-party.core-test
   (:require [clojure.test :refer :all]
-            [clj-pool-party.core :refer :all]))
+            [clj-pool-party.core :as sut]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.generators :as gen])
+  (:import (clojure.lang Ref)
+           (java.util.concurrent Executors)))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;;non-positive max size values should result in an assertion error
+(defspec pool-max-size-pos 10
+  (prop/for-all [max-size (gen/such-that #(<= % 0) gen/small-integer)]
+    (try
+      (sut/build-pool (constantly 5) max-size)
+      false
+      (catch AssertionError e
+        true))))
+
+(defspec max-size-not-exceeded 100
+  (prop/for-all [max-size (gen/such-that pos? gen/nat)
+                 n gen/nat]
+    (let [id-atom (atom 0)
+          sample-gen-fn (fn []
+                          (let [new-id (swap! id-atom inc)]
+                            {:id new-id}))
+          ^Ref pool-ref (sut/build-pool sample-gen-fn max-size)
+          times (mod n 3)]
+      (dotimes [idx (* (inc times) max-size)]
+        (sut/with-object pool-ref identity))
+      (<= @id-atom max-size))))
+
+(defspec max-size-not-exceeded-pmap 100
+  (prop/for-all [max-size (gen/such-that pos? gen/nat)
+                 n gen/nat]
+    (let [id-atom (atom 0)
+          sample-gen-fn (fn []
+                          (let [new-id (swap! id-atom inc)]
+                            {:id new-id}))
+          ^Ref pool-ref (sut/build-pool sample-gen-fn max-size)
+          times (mod n 3)]
+      (dorun
+        (pmap (fn [idx]
+                (sut/with-object pool-ref
+                  (fn [obj]
+                    (Thread/sleep 10))))
+          (range (* (inc times) max-size))))
+      (<= @id-atom max-size))))
+
+(defspec max-size-not-exceeded-vthreads 100
+  (prop/for-all [max-size (gen/such-that pos? gen/nat)
+                 n gen/nat]
+    (let [id-atom (atom 0)
+          sample-gen-fn (fn []
+                          (let [new-id (swap! id-atom inc)]
+                            {:id new-id}))
+          ^Ref pool-ref (sut/build-pool sample-gen-fn max-size)
+          times (mod n 3)
+          vthread-fn (fn []
+                       (sut/with-object pool-ref
+                         (fn [obj]
+                           (Thread/sleep 10))))]
+      (with-open [exec (Executors/newVirtualThreadPerTaskExecutor)]
+        (dotimes [idx (* (inc times) max-size)]
+          (let [vthread (Thread. vthread-fn)]
+            (.submit exec vthread))))
+      (<= 1 @id-atom max-size))))
