494c55d53b3a02e76980a83fa1396745ffbb303b
Stephen Hopper <stephenmhopper@gmail.com>
2023-10-22 13:38:01 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-pool-party
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_pool_party/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-pool-party.core-test
+  (:require [clojure.test :refer :all]
+            [clj-pool-party.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
