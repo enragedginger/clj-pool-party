82e60af62748de84ba1c63a5951c68f745879831
Stephen Hopper <stephenmhopper@gmail.com>
2023-10-26 16:22:56 -0500

    Update version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.enragedginger/clj-pool-party "0.1.0-SNAPSHOT"
+(defproject com.github.enragedginger/clj-pool-party "0.1.0"
   :description "Minimalistic Clojure object pooling library"
   :url "https://github.com/enragedginger/clj-pool-party"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
